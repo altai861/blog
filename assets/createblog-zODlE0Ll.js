f6a2d83ebe2dae2676b176fcfde8a80f5ec0af4e
altai861 <altaizandanbileg@gmail.com>
2023-12-16 11:10:52 +0800

    Updates

---
+++ assets/createblog-zODlE0Ll.js
@@ -0,0 +1,3 @@
+import"./style-tFgqS8q_.js";let e=document.getElementById("app"),t=sessionStorage.getItem("username");document.addEventListener("DOMContentLoaded",()=>{t?e.innerHTML=`
+            <h1>Write yourself out</h1>
+        `:window.location.replace("/")});
