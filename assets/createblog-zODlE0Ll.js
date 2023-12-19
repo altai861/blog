f4dfa2fd4b6f227b8ede29ff982dae3b2959b93c
altai861 <altaizandanbileg@gmail.com>
2023-12-19 17:13:34 +0800

    Updates

--- assets/createblog-zODlE0Ll.js
+++
@@ -1,3 +0,0 @@
-import"./style-tFgqS8q_.js";let e=document.getElementById("app"),t=sessionStorage.getItem("username");document.addEventListener("DOMContentLoaded",()=>{t?e.innerHTML=`
-            <h1>Write yourself out</h1>
-        `:window.location.replace("/")});
--- assets/main-_ofxdmlC.js
+++
@@ -1,5 +0,0 @@
-import"./style-tFgqS8q_.js";let t=sessionStorage.getItem("username"),e=document.getElementById("app");document.addEventListener("DOMContentLoaded",()=>{t?(console.log("You are Altai"),e.innerHTML=`
-      <h1>What is up, Altai?</h1>
-    `):(console.log("You are just reader"),e.innerHTML=`
-      <h1>What is up, ......?</h1>
-    `)});
