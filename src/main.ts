238d5cb5707d2b782011372b4a3d0952ea4bff30
altai.g <altai.g@grapecity.mn>
2025-01-18 11:47:11 +0800

    switched to angular

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,6 @@
-import './style.css'
-import { homePage } from './homepage'
-import { blogPage } from './blog'
-import { newPage } from './new';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-function router() {
-  let path = window.location.hash
-  console.log(path)
-  if (!path) {
-    homePage();
-  } else if (path === "#new") {
-    newPage();
-  } else {
-    blogPage(path.substring(1))
-  }
-}
-
-
-window.addEventListener('hashchange', router);
-
-window.addEventListener("load", router);
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
