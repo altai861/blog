8106950e915b9be64648acd5e72fe0d61e312e52
altai861 <altaizandanbileg@gmail.com>
2025-01-19 21:03:42 +0800

    editor styling fix, and json start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EditorComponent } from "./editor/editor.component";
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EditorComponent],
+  imports: [RouterOutlet, EditorComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'blog';
+  blogs = [];
+
+  constructor(private http: HttpClient) {}
+
+
+  ngOnInit(): void {
+      this.http.get("/meta/blogs.json").subscribe({
+        next: (res: any) => {
+          console.log(res);
+          this.blogs = res.blogs
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      })
+  }
 }
